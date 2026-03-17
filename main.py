8ac10f5e69045b1bab3d3ad32c2c7218c9186a1c
soumayg9673 <soumayg9673@zohomail.in>
2026-03-18 00:05:44 +0530

    add delta time

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    global dt
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,6 +18,8 @@ def main():
 
             if event.type == pygame.QUIT:
                 return
+            
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
