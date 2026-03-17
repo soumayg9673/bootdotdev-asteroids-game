4ceb7251b95bcaffd1a8492d1db530458a7c8f8e
soumayg9673 <soumayg9673@zohomail.in>
2026-03-17 23:20:26 +0530

    first commit for pygame

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            screen.fill("black")
+            pygame.display.flip()
+
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
